0d092b8962f656d8a589889f576dd26e4c26f424
Chris Lutze <clutze@gmail.com>
2023-05-18 17:15:21 -1000

    Updated DDL to match report and added comments

--- SCUBA_DB_DDL.sql
+++ SCUBA_DB_DDL.sql
@@ -1,7 +1,7 @@
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 SET AUTOCOMMIT = 0;
 
@@ -40,27 +40,6 @@ CREATE OR REPLACE TABLE Units (
   PRIMARY KEY (`unit_id`))
   ENGINE = InnoDB;
 
--- Create Gear table
-
-/*CREATE OR REPLACE TABLE Gear (
-  `gear_id` INT NOT NULL AUTO_INCREMENT,
-  `exposure_suit` VARCHAR(45) NULL,
-  `boots` VARCHAR(45) NULL,
-  `gloves` VARCHAR(45) NULL,
-  `hood` VARCHAR(45) NULL,
-  `fins` VARCHAR(45) NULL,
-  `bcd` VARCHAR(45) NULL,             -- bcd stands for Buoyance Control Device
-  `regulator` VARCHAR(45) NULL,
-  `diver_id` INT NOT NULL,
-  PRIMARY KEY (`gear_id`),
-  INDEX `fk_Gear_Divers1_idx` (`diver_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Gear_Divers1`
-    FOREIGN KEY (`diver_id`)
-    REFERENCES `Divers` (`diver_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;*/
-
 -- Create Dives table
 -- SAC stands for Surface Air/Gas Consumption rate which is a normalized gas consumption rate
 
@@ -73,13 +52,13 @@ CREATE OR REPLACE TABLE Dives (
   `duration` INT NOT NULL,
   `start_pressure` INT NULL,
   `end_pressure` INT NULL,
-  `gas_type` VARCHAR(45) NULL,
+  `gas_type` VARCHAR(10) NULL,
   `SAC` DECIMAL(3,1) NULL,
   `weight` INT NULL,
   `water_temperature` INT NULL,
   `visibility` INT NULL,
   `entry_details` VARCHAR(50) NULL,
-  `condition_note` VARCHAR(500) NULL,
+  `condition_note` VARCHAR(100) NULL,
   `note` VARCHAR(2000) NULL,
   `site_rating` INT NULL,
   PRIMARY KEY (`dive_id`),
@@ -97,11 +76,9 @@ CREATE OR REPLACE TABLE Divelogs (
   `divelog_id` INT NOT NULL AUTO_INCREMENT,
   `dive_id` INT NOT NULL,
   `diver_id` INT NOT NULL,
-  /*`gear_id` INT,*/
   INDEX `fk_Dives_has_Divers_Divers1_idx` (`diver_id` ASC) VISIBLE,
   INDEX `fk_Dives_has_Divers_Dives1_idx` (`dive_id` ASC) VISIBLE,
   PRIMARY KEY (`divelog_id`),
-  /*INDEX `fk_DiversToDives_Gear1_idx` (`gear_id` ASC) VISIBLE,*/
   CONSTRAINT `fk_Dives_has_Divers_Dives1`
     FOREIGN KEY (`dive_id`)
     REFERENCES `Dives` (`dive_id`)
@@ -111,12 +88,7 @@ CREATE OR REPLACE TABLE Divelogs (
     FOREIGN KEY (`diver_id`)
     REFERENCES `Divers` (`diver_id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE
-  /*CONSTRAINT `fk_DiversToDives_Gear1`
-    FOREIGN KEY (`gear_id`)
-    REFERENCES `Gear` (`gear_id`)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE*/)
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- Create DivesToDiveSites intersection table
@@ -156,22 +128,9 @@ values ('Golden Arches', 'Kona', 'United States'),
 ('Texas', 'Roatan', 'Honduras'),
 ('Flame Reef', 'Santa Cruz Island', 'United States');
 
-/*INSERT INTO Gear (exposure_suit, boots, gloves, hood, fins, bcd, regulator, diver_id)
-values ('7mm Full Suit', '5mm Boots', '3mm Gloves', '3mm Hood', 'RK3', 'ScubaPro HydrosPro w/ Air2', 'Mares', 1), 
-('Dry Suit', NULL, '3mm Gloves', '3mm Hood', 'RK3', 'ScubaPro HydrosPro w/ Air2', 'Mares', 1),
-('3/4mm Full Suit', NULL, NULL, NULL, 'RK3', 'Mares Rover Pro', 'Mares Rover 2S', 2),
-('Rash Guard', NULL, NULL, NULL, 'RK3', 'ScubaPro HydrosPro w/ Air2', 'Mares', 5);
-*/
-
 INSERT INTO Units (pressure_unit, length_unit, weight_unit, temperature_unit)
 values ('psi', 'ft', 'lb', 'F'),
 ('bar', 'm', 'kg', 'C');
-
-/*INSERT INTO Dives (duration, max_depth, avg_depth, start_pressure, end_pressure, weight, water_temperature, visibility, note, date, SAC, gas_type, entry_details, site_rating)
-values (32, 62, NULL, 3000, 1000, 37, 55, 20, 'The sea floor was covered in brittle star and sea stars. We say a few schools of fish swimming through. Successfully managed to navigate in a half circle back to the boat anchor throughout the site. The kelp was so tall. We saw a bat ray towards the end of the dive cruising around 40 feet deep.',
- '2019-05-26', NULL, 'Air', 'Boat', 3),
-(50, 32, 24, 3200, 1200, 18, 54, 10, 'Swam out along in kelp beds I saw some yellow nudibranches and some white nudibranchs, and a white small slug?. Saw a fair amount of decorator crabs, sea cucumbers, and rock fish, a sea lion, lots of brittle stars. I started to get cold towards the end of the dive but was not shivering.',
- '2021-01-16', NULL, 'Air', 'Shore', 3);*/
 
 INSERT INTO Dives (unit_id, date, max_depth, avg_depth, duration, start_pressure, end_pressure,
 gas_type, SAC, weight, water_temperature, visibility, entry_details, condition_note, note, site_rating)
@@ -180,15 +139,20 @@ VALUES (1, '2019-05-26', 62, NULL, 32, 3000, 1000, 'Air', NULL, 37, 55, 20, 'Boa
 3),
 (1, '2021-01-16', 32, 24, 50, 3200, 1200, 'Air', ((start_pressure-end_pressure)/(duration*(avg_depth/33+1))), 18, 54, 10, 'Shore', 'Some swell on entry',
 'Swam out along in kelp beds I saw some yellow nudibranches and some white nudibranchs, and a white small slug?. Saw a fair amount of decorator crabs, sea cucumbers, and rock fish, a sea lion, lots of brittle stars. I started to get cold towards the end of the dive but was not shivering.',
-3);
+3),
+(2, '1993-09-20', 60, 45, 52, 200, 50, 'EAN32', ((start_pressure-end_pressure)/(duration*(avg_depth/10+1))), 6, 25, 100, 'Boat', 'Strong drift current',
+'This was a drift dive with fairly strong current. We descended to a reef wall and swam through some canyons for the first part of the dive and then entered the current. We followed the reef wall for most of the dive riding the current to the point of the island. I saw a very large rainbow parrot fish and 2 large barracudas. There were very large barrel sponges that I could have fit into. Texas itself was a Sandy patch with very large barrel sponges that looked a bit like tumbleweeds or saguaros. My favorite dive so far. Riding the current and going up and over reef patches was really fun.',
+5);
 
-INSERT INTO Divelogs (dive_id, diver_id/*, gear_id*/)
-values (1, 1/*, 2*/),
-(2, 2/*, NULL*/);
+INSERT INTO Divelogs (dive_id, diver_id)
+values (1, 1),
+(2, 2),
+(3, 3);
 
 INSERT INTO DivesToDiveSites (dive_id, divesite_id)
 values (1, (select divesite_id from DiveSites where site_name = 'Flame Reef')),
-(2, (select divesite_id from DiveSites where site_name = 'Metridium Fields'));
+(2, (select divesite_id from DiveSites where site_name = 'Metridium Fields')),
+(3, 4);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=1;
