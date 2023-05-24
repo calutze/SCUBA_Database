d1f9cb170992585c7ec8b3497f5df91221e44831
Chris Lutze <clutze@gmail.com>
2023-05-24 10:40:15 -0700

    Addd views for Divers, Dives, and DiveSites to properly handle calculated values: SAC, avg_SAC, avg_site_rating, num_dives, total_dive_time. Subsequently removed those values from the original entities.

--- database/SCUBA_DB_DDL.sql
+++ database/SCUBA_DB_DDL.sql
@@ -12,11 +12,23 @@ CREATE OR REPLACE TABLE Divers (
   `diver_id` INT NOT NULL AUTO_INCREMENT,
   `diver_name` VARCHAR(100) NOT NULL UNIQUE,
   `diver_age` TINYINT(3) NOT NULL,
-  `avg_SAC` DECIMAL(3,1) NULL, -- Calculated Value. SAC stands for Surface Air/Gas Consumption rate which is a normalized gas consumption rate
-  `num_dives` INT NULL, -- Calculated Value
-  `total_dive_time` INT NULL, -- Calculated Value
   PRIMARY KEY (`diver_id`))
 ENGINE = InnoDB;
+
+-- Create VIEW of Divers which includes calculated values avg_SAC, num_dives, total_dive_time
+ -- SAC stands for Surface Air/Gas Consumption rate which is a normalized gas consumption rate
+ 
+CREATE OR REPLACE VIEW Divers_View AS
+SELECT *, 
+(SELECT AVG(SAC) FROM Dives_View 
+  JOIN Divelogs ON Dives_View.dive_id = Divelogs.dive_id 
+  WHERE Divelogs.diver_id = Divers.diver_id) AS avg_SAC,
+(SELECT COUNT(dive_id) FROM Divelogs 
+  WHERE Divelogs.diver_id = Divers.diver_id) AS num_dives,
+(SELECT SUM(duration) FROM Dives 
+  JOIN Divelogs ON Dives.dive_id = Divelogs.dive_id 
+  WHERE Divelogs.diver_id = Divers.diver_id) AS total_dive_time
+FROM Divers;
 
 -- Create DiveSites table
 
@@ -25,9 +37,17 @@ CREATE OR REPLACE TABLE DiveSites (
   `site_name` VARCHAR(100) NOT NULL,
   `city` VARCHAR(100) NOT NULL,
   `country` VARCHAR(100) NOT NULL,
-  `avg_site_rating` INT NULL, -- Calculated Value
   PRIMARY KEY (`divesite_id`))
 ENGINE = InnoDB;
+
+-- Create VIEW of DiveSites which includes calculated value avg_site_rating
+
+CREATE OR REPLACE VIEW DiveSites_View AS
+SELECT *, 
+(SELECT CAST(AVG(site_rating) AS DECIMAL(2,1)) FROM Dives 
+      JOIN DivesToDiveSites ON Dives.dive_id = DivesToDiveSites.dive_id 
+      WHERE DivesToDiveSites.divesite_id = DiveSites.divesite_id) AS avg_site_rating
+FROM DiveSites;
 
 -- Create Units Table
 
@@ -52,7 +72,6 @@ CREATE OR REPLACE TABLE Dives (
   `start_pressure` INT NULL,
   `end_pressure` INT NULL,
   `gas_type` VARCHAR(10) NULL,
-  `SAC` DECIMAL(3,1) NULL, -- Calculated Value. SAC stands for Surface Air/Gas Consumption rate which is a normalized gas consumption rate
   `weight` INT NULL,
   `water_temperature` INT NULL,
   `visibility` INT NULL,
@@ -68,6 +87,16 @@ CREATE OR REPLACE TABLE Dives (
     ON UPDATE CASCADE
     ON DELETE RESTRICT)
 ENGINE = InnoDB;
+
+-- Create VIEW of Dives which includes calculated value SAC
+-- SAC stands for Surface Air/Gas Consumption rate which is a normalized gas consumption rate
+
+CREATE OR REPLACE VIEW Dives_View AS
+SELECT *, 
+(CAST(IF(unit_id = (SELECT unit_id FROM Units WHERE pressure_unit = 'psi'),
+        (start_pressure-end_pressure)/(duration*(avg_depth/33+1)),
+        (start_pressure-end_pressure)/(duration*(avg_depth/10+1))) AS DECIMAL(3,1))) AS SAC
+FROM Dives;
 
 -- Create Divelogs intersection table
 
@@ -134,14 +163,14 @@ values ('psi', 'ft', 'lb', 'F'), -- Imperial units
 
 -- Insert sample data into Dives
 INSERT INTO Dives (unit_id, date, max_depth, avg_depth, duration, start_pressure, end_pressure,
-gas_type, SAC, weight, water_temperature, visibility, entry_details, condition_note, note, site_rating)
-VALUES (1, '2019-05-26', 62, NULL, 32, 3000, 1000, 'Air', NULL, 37, 55, 20, 'Boat', 'Mild surge',
+gas_type, weight, water_temperature, visibility, entry_details, condition_note, note, site_rating)
+VALUES (1, '2019-05-26', 62, NULL, 32, 3000, 1000, 'Air', 37, 55, 20, 'Boat', 'Mild surge',
 'The sea floor was covered in brittle star and sea stars. We say a few schools of fish swimming through. Successfully managed to navigate in a half circle back to the boat anchor throughout the site. The kelp was so tall. We saw a bat ray towards the end of the dive cruising around 40 feet deep.',
 3),
-(1, '2021-01-16', 32, 24, 50, 3200, 1200, 'Air', ((start_pressure-end_pressure)/(duration*(avg_depth/33+1))), 18, 54, 10, 'Shore', 'Some swell on entry',
+(1, '2021-01-16', 32, 24, 50, 3200, 1200, 'Air', 18, 54, 10, 'Shore', 'Some swell on entry',
 'Swam out along in kelp beds I saw some yellow nudibranches and some white nudibranchs, and a white small slug?. Saw a fair amount of decorator crabs, sea cucumbers, and rock fish, a sea lion, lots of brittle stars. I started to get cold towards the end of the dive but was not shivering.',
-3),
-(2, '1993-09-20', 60, 45, 52, 200, 50, 'EAN32', ((start_pressure-end_pressure)/(duration*(avg_depth/10+1))), 6, 25, 100, 'Boat', 'Strong drift current',
+4),
+(2, '1993-09-20', 60, 45, 52, 200, 50, 'EAN32', 6, 25, 100, 'Boat', 'Strong drift current',
 'This was a drift dive with fairly strong current. We descended to a reef wall and swam through some canyons for the first part of the dive and then entered the current. We followed the reef wall for most of the dive riding the current to the point of the island. I saw a very large rainbow parrot fish and 2 large barracudas. There were very large barrel sponges that I could have fit into. Texas itself was a Sandy patch with very large barrel sponges that looked a bit like tumbleweeds or saguaros. My favorite dive so far. Riding the current and going up and over reef patches was really fun.',
 5);
 
@@ -153,7 +182,7 @@ values (1, 1),
 
 -- Insert sample data into DivesToDiveSites
 INSERT INTO DivesToDiveSites (dive_id, divesite_id)
-values (1, (select divesite_id from DiveSites where site_name = 'Flame Reef')),
+values (1, (select divesite_id from DiveSites where site_name = 'Metridium Fields')),
 (2, (select divesite_id from DiveSites where site_name = 'Metridium Fields')),
 (3, 4);
 
