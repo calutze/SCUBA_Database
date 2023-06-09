8fcdafd7a7726b37ee53fc8270292d6ef1d83ff5
Chris Lutze <clutze@gmail.com>
2023-06-09 15:28:07 -0700

    Added update column to Divers table and auto-filling for the update form

--- public/js/update_diver.js
+++ public/js/update_diver.js
@@ -1,44 +1,62 @@
 // Based on starter code described in https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-let updateDiverForm = document.getElementById('updateDiver');
+function updateDiver(diverID) {
 
-updateDiverForm.addEventListener("submit", function (e) {
-
-    e.preventDefault();
-
-    let selectDiverId = document.getElementById("mySelect");
-    let selectDiverAge = document.getElementById("selected_diver_age");
-    let updatedDiverName = document.getElementById("updated_diver_name")
-
-    let diverIdValue = selectDiverId.value;
-    let diverAgeValue = selectDiverAge.value;
-    let updatedDiverNameValue = updatedDiverName.value
-
-
-    let data = {
-        diver_id: diverIdValue,
-        diver_name: updatedDiverNameValue,
-        diver_age: diverAgeValue
-    }
-
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "/updateDiver", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-
+    window.scrollTo(0, document.body.scrollHeight);
     
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
+    let updateDiverForm = document.getElementById('update-diver-form');
 
-            updateRow(xhttp.response, diverIdValue);
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 200) {
-            console.log ("There was an error with the input.")
-        }
-    }
+    document.getElementById("updated_diver_name").removeAttribute("disabled");
+    document.getElementById("selected_diver_age").removeAttribute("disabled");
 
-    xhttp.send(JSON.stringify(data));
-})
+    const rowToUpdate = document.querySelector(`[data-value="${diverID}"]`)
+
+    document.getElementById("update_diver_id").value = diverID;
+    
+    let diverName = rowToUpdate.getElementsByClassName("diver-name")[0].textContent;
+    document.getElementById("updated_diver_name").value = diverName
+
+    let diverAge = rowToUpdate.getElementsByClassName("diver-age")[0].textContent;
+    document.getElementById("selected_diver_age").value = diverAge
+
+    updateDiverForm.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        let selectDiverId = document.getElementById("update_diver_id");
+        let selectDiverAge = document.getElementById("selected_diver_age");
+        let updatedDiverName = document.getElementById("updated_diver_name");
+
+        let diverIdValue = selectDiverId.value;
+        let diverAgeValue = selectDiverAge.value;
+        let updatedDiverNameValue = updatedDiverName.value
+
+        let data = {
+            diver_id: diverIdValue,
+            diver_name: updatedDiverNameValue,
+            diver_age: diverAgeValue
+        }
+
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PUT", "/updateDiver", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+
+        
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+                updateRow(xhttp.response, diverIdValue);
+            }
+            else if (xhttp.readyState == 4 && xhttp.status != 200) {
+                console.log ("There was an error with the input.")
+            }
+        }
+
+        xhttp.send(JSON.stringify(data));
+    })
+}
 
 function updateRow(data, diver_id){
     let parsedData = JSON.parse(data);
@@ -51,8 +69,8 @@ function updateRow(data, diver_id){
 
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
-            updateRowIndex.getElementsByTagName("td")[2].innerHTML = parsedData[0].diver_name;
-            updateRowIndex.getElementsByTagName("td")[3].innerHTML = parsedData[0].diver_age;
+            updateRowIndex.getElementsByTagName("td")[3].innerHTML = parsedData[0].diver_name;
+            updateRowIndex.getElementsByTagName("td")[4].innerHTML = parsedData[0].diver_age;
 
         }
     }
