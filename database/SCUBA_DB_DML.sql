1ecc8935ad35ff8f3ae594bf47017e8f0ffe6702
justin-ngo-jn <ngojust@oregonstate.edu>
2023-06-12 21:04:03 -0700

    Updated DML sql file

--- database/SCUBA_DB_DML.sql
+++ database/SCUBA_DB_DML.sql
@@ -29,21 +29,21 @@ SELECT * FROM Divers_View;
 
 -- insert new diver into Divers table
 INSERT INTO Divers (diver_name, diver_age)
-values (:diverNameInput, :ageInput);
+values (:inputDiverName, :inputDiverAge);
 
 -- Show existing values in update diver area
 SELECT diver_name, diver_age FROM Divers WHERE diver_id = :selectedDiverId
 
 -- update diver in Divers table
 UPDATE Divers
-SET diver_name = :diverNameInput, diver_age = :ageInput
-WHERE diver_id = :selectedDiverId
+SET diver_name = :updatedDiverName, diver_age = :selectDiverAge
+WHERE diver_id = :selectDiverId
 
 -- select specific updated diver for AJAX updating
-SELECT * FROM Divers WHERE diver_id = :updatedDiverId
+SELECT * FROM Divers WHERE diver_id = :selectDiverId
 
 -- delete selected diver from Divers table
-DELETE FROM Divers where diver_id = :selectedDiverId
+DELETE FROM Divers where diver_id = :selectDiverId
 
 
 -- Units Page Queries -------------------
@@ -52,14 +52,14 @@ SELECT * FROM Units;
 
 -- insert new unit into Units table
 INSERT INTO Units (pressure_unit, length_unit, weight_unit, temperature_unit)
-VALUES (:pressureInput, :lengthInput, :weightUnitInput, :temperatureInput)
+VALUES (:inputPressureUnit, :inputLengthUnit, :inputWeightUnit, :inputTemperatureUnit)
 
 -- Show existing values in update unit area
 SELECT pressure_unit, length_unit, weight_unit, temeprature_unit FROM Units WHERE unit_id = :selectedUnitId
 
 -- update unit in Units table
 UPDATE Units
-SET pressure_unit = :pressureInput, length_unit = :lengthInput, weight_unit = :weightInput, temperature_unit = :temperatureInput
+SET pressure_unit = :updatePressureUnit, length_unit = :updateLengthUnit, weight_unit = :updateWeightUnit, temperature_unit = :updateTemperatureUnit
 WHERE unit_id = :selectedUnitId
 
 
@@ -71,7 +71,7 @@ JOIN Units on Dives_View.unit_id = Units.unit_id;
 
 -- insert new dive entry into Dives table
 INSERT INTO Dives (unit_id, date, max_depth, avg_depth, duration, start_pressure, end_pressure, gas_type, weight, water_temperature, visibility, entry_details, condition_note, note, site_rating) 
-VALUES (:unit_idInput, :dateInput, :max_depthInput, :avg_depthInput, :durationInput, :start_pressureInput, :end_pressureInput, :gas_typeInput, :weightInput, :water_temperatureInput, :visibilityInput, :entry_detailsInput, :condition_noteInput, :noteInput, :site_ratingInput)
+VALUES (:inputUnits, :inputDate, :inputMaxDepth, :inputAvgDepth, :inputDuration, :inputStartPressure, :inputEndPressure, :inputGasType, :inputWeight, :inputWaterTemp, :inputVisibility, :inputEntryDetails, :inputConditionNote, :inputNote, :inputSiteRating)
 
 -- show existing values in update dives area
 SELECT dive_id, unit_name as units, date, max_depth, avg_depth, duration, start_pressure, end_pressure, SAC, gas_type, weight, water_temperature, visibility, entry_details, condition_note, note, site_rating 
@@ -81,7 +81,7 @@ WHERE Dives_View.dive_id = :selectedDiveId
 
 -- update dives in Dives table
 UPDATE Dives 
-SET unit_id = :unit_idInput, date = :dateInput, max_depth = :max_depthInput, avg_depth = :avg_depthInput, duration = :durationInput, start_pressure = :start_pressureInput, end_pressure = :end_pressureInput, gas_type = :gas_typeInput, weight = :weightInput, water_temperature = ?, visibility = ?, entry_details = ?, condition_note = ?, note = ?, site_rating = ? 
+SET unit_id = :updateUnits, date = :updateDate, max_depth = :updateMaxDepth, avg_depth = :updateAvgDepth, duration = :updateDuration, start_pressure = :updateStartPressure, end_pressure = :updateEndPressure, gas_type = :updateGasType, weight = :updateWeight, water_temperature = :updateWaterTemp, visibility = :updateVisibility, entry_details = :updateEntryDetails, condition_note = :updateConditionNote, note = :updateNote, site_rating = :updateSiteRating 
 WHERE dive_id = :updateDiveId
 
 -- delete selected dive from Dives table
@@ -94,14 +94,14 @@ SELECT * from DiveSites;
 
 -- insert new location into DiveSites table
 INSERT INTO DiveSites (site_name, city, country)
-values (:siteInput, :cityInput, :countryInput);
+values (:inputSiteName, :inputCity, :inputCountry);
 
 -- show existing values in update dive sites area
 SELECT site_name, city, country FROM DiveSites WHERE divesite_id = :selectedDiveSiteId
 
 -- update dive site in Dive Sites table
 UPDATE DiveSites
-SET site_name = :siteInput, city = :cityInput, country = :countryInput WHERE divesite_id = selectedDiveSiteId
+SET site_name = :updateSiteName, city = :updateCity, country = :updateCountry WHERE divesite_id = selectedDiveSiteId
 
 -- delete selected dive site from DiveSites table
 DELETE FROM DiveSites WHERE divesite_id = :selectedDiveSiteId
@@ -109,15 +109,15 @@ DELETE FROM DiveSites WHERE divesite_id = :selectedDiveSiteId
 
 -- Dive Logs Page Queries -------------------------
 -- show all data from DiveLogs table
-SELECT divelog_id, Divelogs.dive_id, date, max_depth, duration, Divers.dive_id, diver_name 
+SELECT divelog_id, Divelogs.dive_id, DATE_FORMAT(date, '%d-%M-%Y') as date, max_depth, duration, Divers.diver_id, diver_name 
 FROM Divelogs 
-JOIN Divers ON Divelogs.diver_id = Divers.diver_id 
+LEFT JOIN Divers ON Divelogs.diver_id = Divers.diver_id 
 JOIN Dives ON Divelogs.dive_id = Dives.dive_id 
 ORDER BY divelog_id;
 
 -- insert new intersection between Dives and Divers
 INSERT INTO DiveLogs (dive_id, diver_id)
-values (:diveIdInput, :diverIdInput)
+values (:inputDive, :inputDiver)
 
 -- show existing values in update dive logs area
 SELECT divelog_id, Divelogs.dive_id, date, max_depth, duration, Divers.dive_id, diver_name 
@@ -129,26 +129,30 @@ ORDER BY divelog_id;
 
 -- update dive log in dive logs table
 UPDATE DiveLogs
-SET dive_id = :diveIdInput, diver_id = :diverIdInput WHERE divelog_id = selectedDiveLogId
+SET dive_id = :updatedDiveId, diver_id = :updatedDiverId WHERE divelog_id = selectDivelogId
 
-DELETE FROM DiveLogs WHERE divelog_id = :selectedDiveLogId
+DELETE FROM DiveLogs WHERE divelog_id = :selectDivelogId
 
 
 -- Dives-to-Dive Sites Page Queries --------------------
 -- show all data from DivesToDiveSites table
-SELECT * from DivesToDiveSites;
+SELECT dives_to_divesites_id, DivesToDiveSites.dive_id, DATE_FORMAT(date, '%d-%M-%Y') as date, max_depth, duration, DiveSites.divesite_id, site_name 
+FROM DivesToDiveSites 
+JOIN DiveSites ON DivesToDiveSites.divesite_id = DiveSites.divesite_id 
+JOIN Dives ON DivesToDiveSites.dive_id = Dives.dive_id 
+ORDER BY dives_to_divesites_id;
 
 -- insert new intersection between Dives and DiveSites
 INSERT INTO DivesToDiveSites (dive_id, divesite_id)
-values (:diveIdInput, :diveSiteIdInput)
+values (:inputDive, :inputSite)
 
 -- show existing values in update dives to dive sites area
-SELECT dive_id, divesite_id FROM DivesToDiveSites WHERE dives_to_divesites_id = selectedDivesToDiveSitesId
+SELECT dive_id, divesite_id FROM DivesToDiveSites WHERE dives_to_divesites_id = selectDivesToDiveSitesId
 
 -- update dives to dive sites table
 UPDATE DivesToDiveSites
-SET dive_id = :diveIdInput, divesite_id = :divesiteIdInput WHERE dives_to_divesites_id = selectedDivesToDiveSitesId
+SET dive_id = :updatedDiveId, divesite_id = :updatedSiteId WHERE dives_to_divesites_id = selectDivesToDiveSitesId
 
 -- delete selected dives to dive site from DiveToDiveSites table
-DELETE FROM DivesToDiveSites WHERE dives_to_divesites_id = :selectedDivesToDiveSitesId
+DELETE FROM DivesToDiveSites WHERE dives_to_divesites_id = :selectDivesToDiveSitesId
 
