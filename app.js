0978743a5b00e8a08e84552617c5d18fb908b8fe
Chris Lutze <clutze@gmail.com>
2023-06-12 22:19:09 -0700

    Added CSS formatting.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 12315;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 12316;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./database/db-connector')
